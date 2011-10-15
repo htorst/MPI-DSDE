7618f669cdc5260f52ff411953f61690faf9ccba
Torsten Hoefler <dsde@unixer.de>
2011-10-15 02:25:37 +0000

    more documentation and some fixes

--- dsde.cpp
+++ dsde.cpp
@@ -1,13 +1,13 @@
 #include <dsde.h>
 
 #include <vector>
+#include <stdlib.h>
 #include <assert.h>
 
 int DSDE_Exchange(void* sendbuf, int *sendcounts, int *sdispls,
                   MPI_Datatype sendtype, int destcount, int *dests,
-                  void* recvbuf, int *recvcounts, int *rdispls,
-                  MPI_Datatype recvtype, int srccount, int *sources,
-                  MPI_Comm comm) {
+                  void** recvbuf, int **recvcounts, MPI_Datatype recvtype, 
+                  int *srccount, int **sources, MPI_Comm comm) {
 
   int p;
   MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -21,31 +21,35 @@ int DSDE_Exchange(void* sendbuf, int *sendcounts, int *sdispls,
   MPI_Aint lb, extent;
   MPI_Type_get_extent(sendtype, &lb, &extent);
 
-  int offset=0;
   for(int i=0; i < destcount; ++i) {
-    MPI_Isend((char*)sendbuf+extent*offset, sendcounts[i], sendtype, dests[i], 999, comm, &reqs[i]);
-    offset += sendcounts[i];
+    MPI_Isend((char*)sendbuf+extent*sdispls[i], sendcounts[i], sendtype, dests[i], 999, comm, &reqs[i]);
   }
 
   assert(sources == NULL); // avoid memory leaks
   assert(recvbuf == NULL); // avoid memory leaks
-  srccount = 0;
+  *srccount = 0;
 
   int rbufsize = 0;
   for(int i=0; i<p; i++) if(rsizes[i]) {
-    srccount++;
+    (*srccount)++;
     rbufsize+=rsizes[i];
   }
 
+  *recvbuf = malloc(rbufsize*extent);
+
   int reqoffset = reqs.size()-1;
-  reqs.resize(reqs.size()+srccount);
-  std::vector<int> sourcesv(srccount);
-  sources = &sourcesv[0];
+  reqs.resize(reqs.size()+*srccount);
+  *sources = (int*)malloc(sizeof(int)* *srccount);
+  *recvcounts = (int*)malloc(sizeof(int)* *srccount);
   
-  offset = 0;
+  int offset = 0;
+  int index = 0;
   for(int i=0; i<p; i++) if(rsizes[i]) {
-    MPI_Irecv((char*)recvbuf + extent*offset, rsizes[i], recvtype, i, 999, comm, &reqs[reqoffset++]);
+    MPI_Irecv((char*)*recvbuf + extent*offset, rsizes[i], recvtype, i, 999, comm, &reqs[reqoffset++]);
     offset += rsizes[i];
+    (*sources)[index] = i;
+    (*recvcounts)[index] = rsizes[i];
+    index++;
   }
 
   MPI_Waitall(reqs.size(), &reqs[0], MPI_STATUSES_IGNORE);
--- dsde.h
+++ dsde.h
@@ -2,10 +2,17 @@
 
 extern "C" {
 
-int DSDE_Exchange(void* sendbuf, int *sendcounts, int *sdispls,
-                  MPI_Datatype sendtype, int destcount, int *dests,
-                  void* recvbuf, int *recvcounts, int *rdispls,
-                  MPI_Datatype recvtype, int srccount, int *sources,
-                  MPI_Comm comm);
+int DSDE_Exchange(void* sendbuf, // the sendbuffer -- provided by user
+                  int *sendcounts,  // number of elements to send to dests[i]
+                  int *sdispls, // displs to send to dests[i]
+                  MPI_Datatype sendtype, // datatype
+                  int destcount, // number of dest. processes
+                  int *dests, // array of dest. processes
+                  void** recvbuf, // receive buffer, allocated by lib
+                  int **recvcounts, // recvcounts, allocated by lib
+                  MPI_Datatype recvtype, // datatype
+                  int *srccount, // number of source processes
+                  int **sources, // array of source processes
+                  MPI_Comm comm); // communicator
 
 }
